0c01f6b765876208ac7e05506e0b40e7291b2ba6
Matt Johnson <matt.w.johnson@gmail.com>
2016-01-03 15:43:49 +0000

    fix line length on programs_controller

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -69,6 +69,7 @@ class ProgramsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def program_params
-      params.require(:program).permit(:start_date, :start_time, :title, :subtitle, :activity_type, :short_description, :long_description, :loc, :mins, :conference_id)
+      params.require(:program).permit(:start_date, :start_time, :title, :subtitle, :activity_type, 
+        :short_description, :long_description, :loc, :mins, :conference_id)
     end
 end
