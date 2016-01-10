80223577c5b5c33d6799a393ca1420c189f38738
Daniel Johnston <macademy@danieljohnston.co.uk>
2016-01-10 22:57:15 +0000

    added token setup for heroku fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Added to resolve Heroku missing token error
+  config.secret_key_base = ENV[“SECRET_KEY_BASE”]
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
