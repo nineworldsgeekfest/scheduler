bf1a77b222db5c97707a20a9014376766d8462a5
Daniel Johnston <macademy@danieljohnston.co.uk>
2016-01-23 16:00:15 +0000

    added failing validations but still fails tests

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -1,3 +1,18 @@
 class Conference < ActiveRecord::Base
   has_many :tracks
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 100 }
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  # validate :end_after_start
+  #
+  # private
+  # def end_after_start
+  #   return if end_date.blank? || start_date.blank?
+  #
+  #   if end_date < start_date
+  #     errors.add(:end_date, "must be after the start date")
+  #   end
+  # end
+
 end
